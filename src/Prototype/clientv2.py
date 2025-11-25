c4d9dc37b83f7566fb34d544b10b1767d95f28ac
DrKNath <friedrich.nathanael@gmail.com>
2025-11-25 12:43:48 +0100

    modified:   src/Prototype/clientv2.py

--- src/Prototype/clientv2.py
+++ src/Prototype/clientv2.py
@@ -1,19 +1,18 @@
 import socket
 import threading
+import re
 
 class Client:
     def __init__(self,name: str , host: str ='0.0.0.0', port: int = 0):
         self.__name = name
         self.__host = host
         self.__port = port
-        self.__list_Routers = []
-        self.__list_Clients = []
-
+        self.__list_route = []
+        self.__list_client = []
+        self.__route = []
 
 
     def start(self):
-
-
 
         #Thread send msg 
         thread_send = threading.Thread(target=self.send_msg)
@@ -21,8 +20,50 @@ class Client:
         thread_send.start()
 
         self.receive_msg()
+    
+
+    def connection(self, host: str, port: int):
+        co = socket.socket()
+        co.connect((host, port))
+        return co
 
 
+    def connection_master(self):
+        co_master = self.connection('192.168.1.30',10000)
+        co_master.send(f"CLIENT::{self.__name}::{self.__host}::{self.__port}".encode('utf-8'))
+        while True:
+            try: 
+                reply = co_master.recv(1024).decode('utf-8')
+                if not reply:
+                    break
+                self.decompilation_msg_master(reply)
+            except:
+                print("Vous avez été déconnecté du master.")
+                break
+    
+    def decompilation_msg_master(self, msg): 
+        pattern =  r'^(CLIENT|ROUTER)\s+(\S+)\s+((?:\d{1,3}\.){3}\d{1,3})\s+(\d+)$'
+        match = re.match(pattern, msg)
+        if match:
+            if match.group(1) == 'CLIENT':
+                self.__list_client.append({
+                    'type': match.group(1),
+                    'name': match.group(2),
+                    'ip': match.group(3),
+                    'port': int(match.group(4))
+                })
+            elif match.group(1) == 'ROUTER':
+                self.__list_route.append({
+                    'type': match.group(1),
+                    'name': match.group(2),
+                    'ip': match.group(3),
+                    'port': int(match.group(4))
+                })
+
+
+    def gen_route(self):
+        pass
+    
     def send_msg(self):
         send_socket = self.connection('192.168.1.30',10001)
         while True:
@@ -34,8 +75,6 @@ class Client:
             except:
                 print("Impossible d'envoyer le message. Vous êtes peut-être déconnecté.")
                 break
-    
-
     
     def receive_msg(self):
         recv_socket = socket.socket()
@@ -51,17 +90,6 @@ class Client:
             except:
                 print("Vous avez été déconnecté du serveur.")
                 break
-    
-
-    def connection(self, host: str, port: int):
-        co = socket.socket()
-        co.connect((host, port))
-        return co
-
-
-    def connection_master(self):
-        co_master = self.connection('192.168.1.30',10000)
-        co_master.send(f"CLIENT::{self.__name}::{self.__host}::{self.__port}".encode('utf-8'))
     
 
 if __name__ == "__main__":
