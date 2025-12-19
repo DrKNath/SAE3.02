ad9be261be3f1b952da92778c9908d1208db144e
DrKNath <friedrich.nathanael@gmail.com>
2025-12-19 11:12:11 +0100

    changement structure pour future Int Graph

--- src/Prototype/client/core/UIHandler.py
+++ src/Prototype/client/core/UIHandler.py
@@ -1,25 +1,29 @@
 class UIHandler:
     def __init__(self, core):
         self.core = core
+        self.ui = None  # TerminalUI ou GUI
 
-    def start(self):
-        while True:
-            message = input(">> ").strip()
-            if message.startswith("/"):
-                self.handle_command(message)
-            else:
-                onion = self.core.onion_router.build_onion(message)
-                if onion:
-                    self.core.network_handler.send_to_first_router(onion)
+    def set_ui(self, ui):
+        self.ui = ui
+
+    def send_message(self, message):
+        route = self.core.onion_router.gen_route()
+        self.core.route = route
+        onion = self.core.onion_router.build_onion(message, route)
+        self.core.network_handler.send_to_first_router(onion)
 
     def handle_command(self, cmd):
         parts = cmd.split()
         match parts:
             case ["/ip", "master", ip]:
                 self.core.master_conn.master_host = ip
-                print(f"[INFO] Master IP défini: {ip}")
+                if self.ui: self.ui.update_master_ip_field(ip)
             case ["/port", "master", port]:
                 self.core.master_conn.master_port = int(port)
-                print(f"[INFO] Master port défini: {port}")
+                if self.ui: self.ui.update_master_port_field(port)
             case _:
-                print("[ERREUR] Commande inconnue.")
+                print("[ERREUR] Commande inconnue")
+
+    def notify_received_message(self, msg):
+        if self.ui:
+            self.ui.display_message(msg)
--- src/Prototype/client/ui/terminal.py
+++ src/Prototype/client/ui/terminal.py
@@ -1,3 +1,24 @@
-# simple terminal UI
-def start_ui(core):
-    core.ui_handler.start()
+class TerminalUI:
+    def __init__(self, ui_handler):
+        self.ui_handler = ui_handler
+        self.master_ip_field = ""
+        self.master_port_field = ""
+        ui_handler.set_ui(self)
+
+    def start(self):
+        print("[INFO] Terminal UI démarrée")
+        while True:
+            message = input(">> ").strip()
+            if message.startswith("/"):
+                self.ui_handler.handle_command(message)
+            else:
+                self.ui_handler.send_message(message)
+
+    def display_message(self, msg):
+        print(f"\n[MSG REÇU] {msg}\n>> ", end="")
+
+    def update_master_ip_field(self, ip):
+        self.master_ip_field = ip
+
+    def update_master_port_field(self, port):
+        self.master_port_field = port
